ecbc6995f2694e40e5ea6a728eb30f24ae5e4935
Gabe Scholz <scholz.gabe@gmail.com>
2017-01-13 12:01:55 -0800

    Update type defs

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import * as types from './typeHelpers';
 import sample from './sample';
 import sampleOne from './sampleOne';
 import asGenerator from './asGenerator';
-import type {$Gen} from './types';
+import {$Gen} from './types';
 
 export {default} from './plugin';
 export {sample, sampleOne, types, asGenerator};
-export type {$Gen};
+export {$Gen};

fbf5f08c435208ce87f6124ac4f26754b65cb11a
Gabe Scholz <scholz.gabe@gmail.com>
2017-01-13 10:31:35 -0800

    Remove HOF for generating expressions

--- src/genFromAST.js
+++ src/genFromAST.js
@@ -3,13 +3,16 @@ import GEN from './GEN_ID';
 
 const {types: t} = babel;
 
+const expression = (str, args) =>
+  babel.template(str)(args).expression;
+
 function typeToGen(obj, params = []) {
   switch (obj.type) {
     case `generator`: {
-      return babel.template(`${GEN}.generator(ARG, CALL)`)({
+      return expression(`${GEN}.generator(ARG, CALL)`, {
         ARG: genFromAST(obj.typeAlias),
         CALL: t.identifier(obj.name),
-      }).expression;
+      });
     }
     case `typeAlias`: {
       let index = -1;
@@ -27,12 +30,12 @@ function typeToGen(obj, params = []) {
 
       if (index === -1) {
         // wrap in a gen.bind so that recursion is lazy
-        return babel.template(`${GEN}.typeAlias(CALL, ARGS)`)({
+        return expression(`${GEN}.typeAlias(CALL, ARGS)`, {
           CALL: t.identifier(obj.name),
           ARGS: t.arrayExpression(
             obj.args.map(a => genFromAST(a, params)),
           ),
-        }).expression;
+        });
       }
 
       return t.identifier(obj.name);
@@ -40,7 +43,7 @@ function typeToGen(obj, params = []) {
     case `object`: {
       const keys = Object.keys(obj.members);
 
-      return babel.template(`${GEN}.object(OBJ)`)({
+      return expression(`${GEN}.object(OBJ)`, {
         OBJ: t.objectExpression(
           keys.map(key =>
             t.objectProperty(
@@ -49,20 +52,20 @@ function typeToGen(obj, params = []) {
             ),
           ),
         ),
-      }).expression;
+      });
     }
     case `typeAliasKeys`:
-      return babel.template(`${GEN}.array(${GEN}.keys(OBJ))`)({
+      return expression(`${GEN}.array(${GEN}.keys(OBJ))`, {
         OBJ: genFromAST(obj.typeAlias),
-      }).expression;
+      });
     case `typeAliasShape`:
-      return babel.template(`${GEN}.shape(OBJ)`)({
+      return expression(`${GEN}.shape(OBJ)`, {
         OBJ: genFromAST(obj.typeAlias),
-      }).expression;
+      });
     case `array`:
-      return babel.template(`${GEN}.array(VAL)`)({
+      return expression(`${GEN}.array(VAL)`, {
         VAL: genFromAST(obj.elementType, params),
-      }).expression;
+      });
     case `literal`:
       return t.identifier(`${GEN}.literal(${JSON.stringify(obj.value)})`);
     case `boolean`:
@@ -72,30 +75,30 @@ function typeToGen(obj, params = []) {
     case `number`:
       return t.identifier(`${GEN}.number()`);
     case `union`:
-      return babel.template(`${GEN}.union(ARR)`)({
+      return expression(`${GEN}.union(ARR)`, {
         ARR: t.arrayExpression(
           obj.entries.map(val => genFromAST(val, params)),
         ),
-      }).expression;
+      });
 
     case `intersection`:
-      return babel.template(`${GEN}.intersection(ARR)`)({
+      return expression(`${GEN}.intersection(ARR)`, {
         ARR: t.arrayExpression(
           obj.entries.map(val => genFromAST(val, params)),
         ),
-      }).expression;
+      });
     case `tuple`:
-      return babel.template(`${GEN}.tuple(ARR)`)({
+      return expression(`${GEN}.tuple(ARR)`, {
         ARR: t.arrayExpression(
           obj.entries.map(val => genFromAST(val, params)),
         ),
-      }).expression;
+      });
     case `function`:
       return t.identifier(`${GEN}.mock()`);
     case `nullable`:
-      return babel.template(`${GEN}.nullable(OBJ)`)({
+      return expression(`${GEN}.nullable(OBJ)`, {
         OBJ: genFromAST(obj.value, params),
-      }).expression;
+      });
     default:
       return t.identifier(`${GEN}.undef()`);
   }
@@ -105,9 +108,9 @@ export default function genFromAST(obj, params) {
   const gen = typeToGen(obj, params);
 
   if (obj.optional === true) {
-    return babel.template(`${GEN}.nullable(OBJ)`)({
+    return expression(`${GEN}.nullable(OBJ)`, {
       OBJ: gen,
-    }).expression;
+    });
   }
 
   return gen;

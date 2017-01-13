a4aa5f90d373b082196050405b7d44dce2cba6e6
Gabe Scholz <scholz.gabe@gmail.com>
2017-01-13 12:13:31 -0800

    Move importable API to ./api

---
+++ src/api.js
@@ -0,0 +1,8 @@
+import * as types from './typeHelpers';
+import sample from './sample';
+import sampleOne from './sampleOne';
+import asGenerator from './asGenerator';
+import {$Gen} from './types';
+
+export {sample, sampleOne, types, asGenerator};
+export {$Gen};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-import * as types from './typeHelpers';
-import sample from './sample';
-import sampleOne from './sampleOne';
-import asGenerator from './asGenerator';
-import {$Gen} from './types';
-
 export {default} from './plugin';
-export {sample, sampleOne, types, asGenerator};
-export {$Gen};

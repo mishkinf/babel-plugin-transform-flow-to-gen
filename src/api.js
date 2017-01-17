dff44bf5b8cc2f498ff1d02daae75d221eda3159
Gabe Scholz <scholz.gabe@gmail.com>
2017-01-16 20:07:24 -0800

    Create new file for  type and don't transpile it

--- src/api.js
+++ src/api.js
@@ -1,7 +1,5 @@
 import * as types from './typeHelpers';
 import sample from './sample';
 import sampleOne from './sampleOne';
-import {$Gen} from './types';
 
 export {sample, sampleOne, types};
-export {$Gen};

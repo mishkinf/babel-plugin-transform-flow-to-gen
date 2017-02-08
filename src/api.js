a0cd7ea39efa16fa238b07aa663a1e771febf17e
Gabe Scholz <scholz.gabe@gmail.com>
2017-02-08 08:57:56 -0800

    Improve api exports

--- src/api.js
+++ src/api.js
@@ -2,4 +2,6 @@ import * as types from './typeHelpers';
 import sample from './sample';
 import sampleOne from './sampleOne';
 
+module.exports = {sample, sampleOne, types};
 export {sample, sampleOne, types};
+export default {sample, sampleOne, types};
